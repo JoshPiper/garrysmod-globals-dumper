71a80e0183ad3661595391264623ee126c506030
JoshPiper <32164094+JoshPiper@users.noreply.github.com>
2023-08-24 01:57:41 +0100

    chore: Move to PostGMLoaded.

--- lua/autorun/dump.lua
+++ lua/autorun/dump.lua
@@ -1,26 +1,44 @@
-local hand = file.Open("globals-dump.txt", "w", "DATA")
+hook.Add("PostGamemodeLoaded", "DumpGlobals", function()
+	print("hello")
 
-local manual = {
-	TOOL = {true, true} -- CL, SV
-}
+	local hand = file.Open("globals-dump.txt", "w", "DATA")
 
-for k, v in pairs(_G) do
-	if not isstring(k) then continue end
+	local manual = {
+		TOOL = {true, true} -- CL, SV
+	}
 
-	hand:Write(k)
-	hand:Write("\n")
-	hand:Flush()
-end
-for k, v in pairs(manual) do
-	if (CLIENT and v[1]) or (SERVER and v[2]) then
+	for k, v in pairs(_G) do
+		if not isstring(k) then continue end
+
 		hand:Write(k)
 		hand:Write("\n")
 		hand:Flush()
 	end
-end
+	for k, v in pairs(manual) do
+		if (CLIENT and v[1]) or (SERVER and v[2]) then
+			hand:Write(k)
+			hand:Write("\n")
+			hand:Flush()
+		end
+	end
 
-hand:Close()
+	hand:Close()
 
-if engine and engine.CloseServer then
-	engine.CloseServer()
-end
+	if engine and engine.CloseServer then
+		timer.Simple(0, engine.CloseServer)
+		--timer.Simple(1, engine.CloseServer)
+		--timer.Simple(2, engine.CloseServer)
+		--engine.CloseServer()
+		--engine.CloseServer()
+		--engine.CloseServer()
+		--engine.CloseServer()
+		--engine.CloseServer()
+		--engine.CloseServer()
+	end
+end)
+
+--timer.Simple(10, function()
+--	print("timer")
+--	engine.CloseServer()
+--end)
+
