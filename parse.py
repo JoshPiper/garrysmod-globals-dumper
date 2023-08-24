477cfcbff404453379b1c617d29d9317a6c6a3ad
JoshPiper <32164094+JoshPiper@users.noreply.github.com>
2023-08-24 16:05:45 +0100

    feat: Add output parser / sorter / uniqueness.

---
+++ parse.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+path = Path(r"C:\Users\joshu\Downloads\dump.txt")
+out = set()
+
+with path.open("r", encoding="utf8") as file:
+	in_block = False
+	for line in file:
+		line = line.strip()
+
+		if line == "START GLOBALS BLOCK":
+			in_block = True
+		elif line == "END GLOBALS BLOCK":
+			in_block = False
+		elif in_block:
+			out.add(line)
+
+out = [glob for glob in out]
+out.sort()
+
+for glob in out:
+	print(glob)
